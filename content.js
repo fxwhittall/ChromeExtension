df997622a3790d85d0367326afd9e5208468ec0f
fxwhittall <fxwhittall@wpi.edu>
2020-06-15 16:55:43 -0400

    added sound on click

--- content.js
+++ content.js
@@ -1,1 +1,4 @@
-document.addEventListener('scroll', () => alert('Watch out, it is DJ Khaled!'));
+//document.addEventListener('scroll', () => alert('Watch out, it is DJ Khaled!'));
+var click = new Audio(chrome.runtime.getURL("Button_Push.mp3"));
+
+document.addEventListener('click', () => click.play());
