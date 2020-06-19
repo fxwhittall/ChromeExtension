353c4b5879ee4aaae4027218d5520a2b2042722d
fxwhittall <fxwhittall@wpi.edu>
2020-06-19 10:02:30 -0400

    added more buttons for style

--- popup.js
+++ popup.js
@@ -1,8 +1,30 @@
-document.querySelector('#changeBtn').addEventListener('click', () => {
-	console.log('button clicked');
+document.querySelector('#whiteBtn').addEventListener('click', () => {
+	console.log('white button clicked');
+	changeColor(white);
+});
 
+document.querySelector('#blackBtn').addEventListener('click', () => {
+	console.log('black button clicked');
+	changeColor(black);
+});
+
+document.querySelector('#greyBtn').addEventListener('click', () => {
+	console.log('grey button clicked');
+	changeColor('#7e7e7e');
+});
+
+document.querySelector('#blueBtn').addEventListener('click', () => {
+	console.log('blue button clicked');
+	changeColor('#80b4f7');
+});
+
+document.querySelector('#changeBtn').addEventListener('click', () => {
+	console.log('custom button clicked');
+	changeColor(document.querySelector('#colorbox').value);
+});
+
+function changeColor(color) => {
 	//read selected color and store it
-	const color = document.querySelector('#colorbox').value;
 	chrome.storage.local.set({color});
 
 	//get current tab and change the background color
@@ -11,4 +33,4 @@ document.querySelector('#changeBtn').addEventListener('click', () => {
 	}, tabs => {
 		tabs.forEach(tab => chrome.tabs.sendMessage(tab.id, {color}));
 	});
-});
+}
