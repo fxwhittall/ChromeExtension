8fa55384c0070f3a2e40fb03c49102ca0168bc74
fxwhittall <fxwhittall@wpi.edu>
2020-06-23 15:24:12 -0400

    color box works, now to add buttons

--- content.js
+++ content.js
@@ -2,15 +2,13 @@
 var click = new Audio(chrome.runtime.getURL("Button_Push.mp3"));
 document.addEventListener('click', () => click.play());
 
-chrome.storage.local.get('color', (response) => {
-	if(response.color){
-		document.body.style.backgroundColor = response.color;
-	}
-})
+// chrome.storage.local.get('color', (response) => {
+// 	if(response.color){
+// 		document.body.style.backgroundColor = response.color;
+// 	}
+// })
 
 //color change
-chrome.runtime.onMessage.addListener(function(request) {
-	if(request.color){
-		document.body.style.backgroundColor = request.color;
-	}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+	document.body.style.backgroundColor = message.input;
 });
--- popup.js
+++ popup.js
@@ -1,36 +1,33 @@
-document.querySelector('#whiteBtn').addEventListener('click', () => {
-	console.log('white button clicked');
-	changeColor(white);
-});
+// document.querySelector('#greyBtn').addEventListener('click', () => {
+// 	console.log('grey button clicked');
+// 	changeColor('#7e7e7e');
+// });
 
-document.querySelector('#blackBtn').addEventListener('click', () => {
-	console.log('black button clicked');
-	changeColor(black);
-});
+// document.querySelector('#blueBtn').addEventListener('click', () => {
+// 	console.log('blue button clicked');
+// 	changeColor('#80b4f7');
+// });
 
-document.querySelector('#greyBtn').addEventListener('click', () => {
-	console.log('grey button clicked');
-	changeColor('#7e7e7e');
-});
+document.addEventListener('DOMContentLoaded', () => {
+	var change = document.getElementById("changeBtn");
+	var whBtn = document.getElementById("whiteBtn");
+	var blkBtn = document.getElementById("blackBtn");
+	change.addEventListener('click', () => {
+		let color = document.getElementById("colorbox").value;
+		changeColor(color);
+	});
+// 	whBtn.addEventListener('click', () => {
+// 	 	changeColor('#ffffff');
+// 	});
+})
 
-document.querySelector('#blueBtn').addEventListener('click', () => {
-	console.log('blue button clicked');
-	changeColor('#80b4f7');
-});
-
-document.querySelector('#changeBtn').addEventListener('click', () => {
-	console.log('custom button clicked');
-	changeColor(document.querySelector('#colorbox').value);
-});
 
 function changeColor(color){
-	//read selected color and store it
-	chrome.storage.local.set({color});
-
 	//get current tab and change the background color
 	chrome.tabs.query({
 		active: true, currentWindow: true
-	}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {color});
+	}, 
+	(tabs) => {
+		chrome.tabs.sendMessage(tabs[0].id, {input: color})
 	});
 }
